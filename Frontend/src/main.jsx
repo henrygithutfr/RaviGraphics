04998019790cd31b97004de8033f02ad784f053d
Henry <harshavardhanfr@gmail.com>
2026-05-19 14:50:02 +0530

    Fixed Cors and added more Credentials

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,9 +5,6 @@ import App from './App.jsx'
 
 import axios from 'axios';
 axios.defaults.withCredentials = true;
-axios.get(url, {
-  withCredentials: true
-});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
