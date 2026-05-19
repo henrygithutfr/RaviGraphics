4580eca6b719848e8f5cba12f2276dd72fb3ba6b
Henry <harshavardhanfr@gmail.com>
2026-05-19 15:03:08 +0530

    Testing cors

--- Backend/index.js
+++ Backend/index.js
@@ -44,15 +44,17 @@ const app = express();
 //   }),
 // );
 
-app.use(cors({
-  origin: [
-    "https://ravi-graphics.vercel.app/",
-    "http://localhost:5173"
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+// app.use(cors({
+//   origin: [
+//     "https://ravi-graphics.vercel.app/",
+//     "http://localhost:5173"
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
